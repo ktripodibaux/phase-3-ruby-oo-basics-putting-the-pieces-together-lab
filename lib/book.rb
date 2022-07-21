dbbbcd9108c763bbbe4eec30bf579c8943e6fdba
Kurt Baux <ktripodibaux@gmail.com>
2022-07-21 11:04:18 -0400

    commit tree 246b1bee3d239ab41bf40e391ca2e4e1a05e0fe0

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,32 @@
+class Book
 
+    attr_accessor :title, :author, :page_count, :genre
+
+    # def initialize(title, author_name, page_count, genre)
+    #     @title = title
+    #     @author_name = author_name
+    #     @page_count = page_count
+    #     @genre = genre
+    # end
+
+    def initialize(title)
+        @title = title
+    end
+
+    def turn_page
+        puts "Flipping the page...wow, you read fast!"
+    end
+
+
+
+
+    
+
+#     def initialize(attributes = {})
+#         attributes.each do |attribute, value|
+#         self.send("#{attribute}=", value)
+#         end
+#   end
+
+end
 
